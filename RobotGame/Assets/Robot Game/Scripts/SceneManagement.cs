742a7d4b1fb548513ba6c09d0a86f79605b613e1
LARRYKEMMO <larryloickemmotejiokem@gmail.com>
2023-05-30 05:16:53 -0300

    Commit Final Work Larry

---
+++ RobotGame/Assets/Robot Game/Scripts/SceneManagement.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine.UI;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("Level1");
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    
+}
